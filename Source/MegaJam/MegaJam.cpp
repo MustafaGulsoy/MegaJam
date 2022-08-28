e23dcecf42938b0e293cfb713f196300e3f4e477
Mustafa Gulsoy <mustafa30045@gmail.com>
2022-08-28 23:03:24 +0300

    first commit

---
+++ Source/MegaJam/MegaJam.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MegaJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MegaJam, "MegaJam" );
+ 
---
+++ Source/MegaJam/MegaJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MegaJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMegaJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMegaJamGameMode();
+};
+
+
+
